9fac47746c6a5d373b59970f03d28f0dce19f1f4
DMITRY LOKHMATOV <maloll777@gmail.com>
2021-03-02 23:43:09 +0300

    добавил комментарии в код добавил отдельные функции для изменения свойсва объекта label(для изоляции объекта)

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
-from  kivy.properties import ObjectProperty
 import sqlite3
 
 
 def get_item_info(id : str) :
+# ищем наименование товара по его ID
+# Возвращает картеж для найденого товара из двух записей (наименование, описание)
+# или [0]
 
-    if not id :
-        return 0
+    if not id  or not id.isdigit():
+        # проверка переданного id на пустоту
+        # проверка на отсутсвие букв в id товара
+        return [0]
     con = sqlite3.connect('./TRIOL.db')
     cursor = con.cursor()
     cursor.execute('select name, description FROM Product where item_number = '+ id)
@@ -17,13 +21,15 @@ def get_item_info(id : str) :
 
 
 class Container(GridLayout) :
-    label = ObjectProperty()
-    textinput = ObjectProperty()
 
     def find_item(self, id : str):
 
         result = get_item_info(id)
-        return result[0] if result else 'No find'
+        return result[0] if result[0] else 'No find'
+
+    def set_label_text(self, text : list, label : object):
+    # функция устанавливаает свойство text объекта label
+        label.text = text
 
 
 class MyApp(App) :
